4fa1c9d30e085260be14f83b5d09ae95294d2034
David Wimsey <david@wimsey.us>
2017-10-09 01:45:28 -0400

    Initial website for shackspace hosting

---
+++ src/main/java/com/shackspacehosting/website/WebrootApplication.java
@@ -0,0 +1,12 @@
+package com.shackspacehosting.website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebrootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebrootApplication.class, args);
+	}
+}
